b20b6dbc9a4a31b0a275b8df57982790d7a7131d
T6970 <tenaldkyf315@gmail.com>
2025-10-17 11:41:39 +0000

    Minor refactor

--- js/script.js
+++ js/script.js
@@ -1,129 +1,54 @@
-import { element } from './elements.js';
-import { mouse } from './input.js';
+import { element }  from './element.js';
+import { darkMode } from './style.js';
 
-const fileName = element.inputField("Untitled", { x: 10, y: 10 }, { width: 200, height: 35 });
-const svg      = document.getElementById("svgContainer")
+const svg       = document.getElementById('canvas' );
+const toolbar   = document.getElementById('toolbar');
+let   fileTitle = '';
 
-element.button("File", { x: 10, y: 55 }, { width: 60, height: 30 }, () => {
+svg.style.position = 'absolute';
+svg.style.left     = '0px';
+svg.style.top      = '0px';
+svg.style.width    = '99%';
+svg.style.height   = '99%';
+svg.style.overflow = 'hidden';
+
+const fileOp = toolbar.children[0];
+fileOp.style.position = 'absolute';
+fileOp.style.left     = '10px';
+fileOp.style.top      = '5px';
+fileOp.style.width    = '30px';
+fileOp.style.height   = '30px';
+fileOp.style.cursor   = 'pointer';
+
+fileOp.onclick = (e) => {
     element.contextMenu(
+        toolbar,
+        { x: 0, y: 30 },
+        { width: 150, height: 30 },
         [
-            { label: "New" , action: () => { 
-                fileName.value = ""; 
-                svg.innerHTML  = "";
-            } },
-
-            { label: "Open", action: () => { 
-                const fileUpload = document.createElement('input');
-                fileUpload.type = 'file';
-                fileUpload.accept = '.svg';
-                fileUpload.onchange = (e) => {
-                    const file = e.target.files[0];
-                    const reader = new FileReader();
-                    reader.onload = (e) => {
-                        svg.innerHTML = e.target.result;
-                        fileName.value = file.name;
-                    };
-                    reader.readAsText(file);
-                };
-                fileUpload.click();
-            } },
-
-            { label: "Save", action: () => {
-                console.log("Saving file:", fileName.value);
-                const fileExport = new Blob(
-                    [svg.innerHTML], 
-                    { type: 'image/svg+xml' }
-                );
-                const a = document.createElement('a');
-                a.href = URL.createObjectURL(fileExport);
-                a.download = fileName.value.endsWith('.svg') ? fileName.value : fileName.value + '.svg';
-                a.click();
-            } },
-        ],
-        { x: 10, y: 90 }
-    );
-});
-
-element.button("Insert", { x: 79, y: 55 }, { width: 60, height: 30 }, () => {
-    element.contextMenu(
-        [
-            {label: "Shapes",action: () => {
-                element.contextMenu(
-                    [
-                        { label: "Rectangle", action: () => {
-                            const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-                            rect.setAttribute("x", mouse.x);
-                            rect.setAttribute("y", mouse.y);
-                            rect.setAttribute("width", 100);
-                            rect.setAttribute("height", 50);
-                            rect.setAttribute("fill", "transparent");
-                            rect.setAttribute("stroke", "black");
-                            svg.appendChild(rect);
-                        } },
-
-                        { label: "Circle", action: () => {
-                            const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-                            circle.setAttribute("cx", mouse.x);
-                            circle.setAttribute("cy", mouse.y);
-                            circle.setAttribute("r", 30);
-                            circle.setAttribute("fill", "transparent");
-                            circle.setAttribute("stroke", "black");
-                            svg.appendChild(circle);
-                        } },
-
-                        { label: "Line", action: () => {
-                            const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-                            line.setAttribute("x1", mouse.x);
-                            line.setAttribute("y1", mouse.y);
-                            line.setAttribute("x2", mouse.x + 100);
-                            line.setAttribute("y2", mouse.y);
-                            line.setAttribute("stroke", "black");
-                            svg.appendChild(line);
-                        } }
-                    ],
-                    { x: 80, y: 90 }
-                )
-            }},
-
-            { label: "Text", action: () => {
-                const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-                text.setAttribute("x", mouse.x);
-                text.setAttribute("y", mouse.y);
-                text.setAttribute("fill", "black");
-                text.textContent = "Sample Text";
-                svg.appendChild(text);
-            }},
-
-            { label: "Image", action: () => {
-                const url = prompt("Enter image URL:");
-                if (url) {
-                    const img = document.createElementNS("http://www.w3.org/2000/svg", "image");
-                    img.setAttribute("href", url);
-                    img.setAttribute("x", mouse.x);
-                    img.setAttribute("y", mouse.y);
-                    img.setAttribute("width", 100);
-                    img.setAttribute("height", 100);
-                    svg.appendChild(img);
+            {
+                text: 'New File',
+                onClick: () => {
+                    setFileTitle('Untitled');
                 }
-            }}
-        ],
-        { x: 80, y: 90 }
-    )
-});
+            },
+            {
+                text: 'Open File',
+                onClick: () => {
+                    alert('Open File clicked');
+                }
+            },
+            {
+                text: 'Save File',
+                onClick: () => {
+                    alert('Save File clicked');
+                }
+            }
+        ]
+    );
+}
 
-element.button("Edit", { x: 148, y: 55 }, { width: 60, height: 30 }, () => {
-    element.contextMenu(
-        [
-            { label: "Undo", action: () => { document.execCommand('undo'); } },
-            { label: "Redo", action: () => { document.execCommand('redo'); } },
-            { label: "Cut", action: () => { document.execCommand('cut'); } },
-            { label: "Copy", action: () => { document.execCommand('copy'); } },
-            { label: "Paste", action: () => { document.execCommand('paste'); } },
-            { label: "Delete", action: () => {
-                const selected = svg.querySelectorAll('.selected');
-                selected.forEach(elem => elem.remove());
-            } }
-        ],
-        { x: 150, y: 90 }
-    )
-});
+export const setFileTitle = (title) => {
+    fileTitle = title;
+    document.title = `My App - ${fileTitle}`;
+};
