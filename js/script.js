9ae3dcdcb8df97995da9045b390f8eda09575630
T6970 <tenaldkyf315@gmail.com>
2025-12-09 17:49:21 +0800

    This is for Onlice

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 import { Nqde } from 'https://cdn.jsdelivr.net/gh/T6970/Nqde@main/src/index.js'
 
 Nqde.newCard("test")
-Nqde.setContent("test", "<h1>Hello, Nqde!</h1><p>This is a test card.</p>")
+Nqde.setContent("test", "<h1>Onlice</h1><p>Test</p>")
