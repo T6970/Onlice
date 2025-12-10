be0052d5b7006fa8512a7b0f62f744044f6d0a35
T6970 <tenaldkyf315@gmail.com>
2025-12-10 20:50:53 +0800

    Adjust the main to fit under toolbar

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@ Nqde.resizeCard("toolbar", (window.innerWidth - 80), 60)
 Nqde.moveCard("toolbar", 20 , 20)
 
 Nqde.newCard("main")
-Nqde.resizeCard("main", (window.innerWidth - 80), (window.innerHeight - 140))
-Nqde.moveCard("main", 20 , 100)
+Nqde.resizeCard("main", (window.innerWidth - 80), (window.innerHeight - 200))
+Nqde.moveCard("main", 20 , 140)
