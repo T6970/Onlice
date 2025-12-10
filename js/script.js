3f017cc20d251149be16862e05bc3ec67c8dfbb3
T6970 <tenaldkyf315@gmail.com>
2025-12-10 19:03:07 +0800

    Added main card

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 import { Nqde } from 'https://cdn.jsdelivr.net/gh/T6970/Nqde@main/src/index.js'
 console.log(Nqde)
 
-Nqde.newCard("test")
-Nqde.resizeCard("test", (window.innerWidth - 80), 60)
-Nqde.moveCard("test", 20 , 20)
+Nqde.newCard("toolbar")
+Nqde.resizeCard("toolbar", (window.innerWidth - 80), 60)
+Nqde.moveCard("toolbar", 20 , 20)
+
+Nqde.newCard("main")
+Nqde.resizeCard("main", (window.innerWidth - 80), (window.innerHeight - 140))
+Nqde.moveCard("main", 20 , 100)
