71e202d96df7c396d6e5bd5154903c14dcd8f229
T6970 <tenaldkyf315@gmail.com>
2025-12-11 21:02:05 +0800

    Moved layout logic to layout.js

---
+++ js/layout.js
@@ -0,0 +1,10 @@
+import { Nqde } from 'https://cdn.jsdelivr.net/gh/T6970/Nqde@main/src/index.js'
+
+Nqde.newCard("toolbar")
+Nqde.resizeCard("toolbar", (window.innerWidth - 80), 60)
+Nqde.moveCard("toolbar", 20 , 20)
+Nqde.setContent("toolbar", "<p>File</p>")
+
+Nqde.newCard("main")
+Nqde.resizeCard("main", (window.innerWidth - 80), (window.innerHeight - 200))
+Nqde.moveCard("main", 20 , 140)
--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-import { Nqde } from 'https://cdn.jsdelivr.net/gh/T6970/Nqde@main/src/index.js'
-console.log(Nqde)
-
-Nqde.newCard("toolbar")
-Nqde.resizeCard("toolbar", (window.innerWidth - 80), 60)
-Nqde.moveCard("toolbar", 20 , 20)
-
-Nqde.newCard("main")
-Nqde.resizeCard("main", (window.innerWidth - 80), (window.innerHeight - 200))
-Nqde.moveCard("main", 20 , 140)
+import './layout.js'
